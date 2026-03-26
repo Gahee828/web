60a1f3447daa6382ce3343ea29ecc018fdfef077
김가희 <mrmrmr90@gmail.com>
2026-03-26 09:47:37 +0900

    3월25일

---
+++ script/listArray.js
@@ -0,0 +1,68 @@
+let listArray = [
+    {
+        pid:0,
+        pName:"Starter Feeding Set",
+        pDesc:"아기의 첫 식사를 부드럽게 시작하는 올인원 이유식 세트",
+        pPrice:"59000",
+        pThumbnail:"list-starter-feeding-set.jpg",
+        pLike:false
+    },
+    {
+        pid:1,
+        pName:"Silicone Suction Bowl",
+        pDesc:"넘어지지 않아 더욱 안심되는, 안정적인 이유식 볼",
+        pPrice:"19000",
+        pThumbnail:"list-Silicone-Suction-Bowl.jpg",
+        pLike:false
+
+    },
+    {
+        pid:2,
+        pName:"Soft Grip Spoon & Fork Set",
+        pDesc:"작은 손에도 편안하게, 스스로 먹는 연습을 돕는 커트러리 ",
+        pPrice:"15000",
+        pThumbnail:"list-Soft-Grip-Spoon&Fork-Set.jpg",
+        pLike:false
+    },
+    {
+        pid:3,
+        pName:"Divided Plate",
+        pDesc:"음식을 깔끔하게 나누어 담는 귀여운 칸 나눔 플레이트",
+        pPrice:"22000",
+        pThumbnail:"list-Divided-Plate.jpg",
+        pLike:false
+    },
+    {
+        pid:4,
+        pName:"Training Cup",
+        pDesc:"처음 마시는 연습을 위한 부드럽고 안전한 트레이닝 컵",
+        pPrice:"59000",
+        pThumbnail:"list-Training-Cup.jpg",
+        pLike:false
+    },
+    {
+        pid:5,
+        pName:" Silicone Bib",
+        pDesc:"흘림까지 깔끔하게 잡아주는 실용적인 실리콘 턱받이",
+        pPrice:"59000",
+        pThumbnail:"list-Silicone-Bib.jpg",
+        pLike:false
+    },
+    {
+        pid:6,
+        pName:"Snack Tray Set",
+        pDesc:"간식을 담아 언제 어디서나 편하게 즐기는 트레이 세트",
+        pPrice:"21000",
+        pThumbnail:"list-Snack-Tray-Set.jpg",
+        pLike:false
+    },
+    {
+        pid:7,
+        pName:"Ice Cube Tray",
+        pDesc:"귀여운 모양으로 재미를 더하는 이유식 큐브 트레이",
+        pPrice:"14000",
+        pThumbnail:"list-Ice-Cube-Tray.jpg",
+        pLike:false
+    },
+
+]
---
+++ script/toWon.js
@@ -0,0 +1,3 @@
+function toWon(number) {
+    return number.toLocaleString('ko-KR');
+}
